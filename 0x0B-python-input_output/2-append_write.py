4fc4f3cbce27a0f0f33eb507842197394609e564
idowuomidoyin <idowuomidoyin@yahoo.com>
2023-06-13 10:53:33 -0700

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+author- omidoyin
+Contains the function "append_wrtie"
+"""
+
+
+def append_write(filename="", text=""):
+    """returns the number of chars appended to "filename" from "text" """
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
