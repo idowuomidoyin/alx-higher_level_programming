afdd01cd1a8fea4f0419159aee83ee442f1223ff
idowuomidoyin <idowuomidoyin@yahoo.com>
2023-08-20 19:53:52 -0700

    0-select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,23 +5,10 @@ dta
 authr- omidoyin"""
 
 import sys
-import MySQLdb as db
-
-
-def connect_and_query() -> None:
-
-    """Connect to the database and execute query"""
-    try:
-        cnx = db.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
-        cursor.execute('SELECT * FROM states ORDER BY `id` ASC;')
-        states = cursor.fetchall()
-
-        for state in states:
-            print(state)
-    except Exception as e:
-        return (e)
-
+import MySQLdb
 
 if __name__ == "__main__":
-    connect_and_query()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
