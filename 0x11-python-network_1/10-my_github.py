dd7fa519e9393eaad05a9d9150e48ad593f24971
idowuomidoyin <idowuomidoyin@yahoo.com>
2023-09-01 05:21:59 -0700

    grow

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Takes my Github creds (username & password)
+and uses the Github API to display my ID
+author-omidoyin
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    token = HTTPBasicAuth(username, password)
+    request = requests.get('https://api.github.com/user', auth=token)
+    print(request.json().get('id'))
---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Takes in a URL, send a request and displays
+the value of the variable X-Request-ID
+author-omidoyin
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
